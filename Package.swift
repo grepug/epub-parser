b5bcd0078ceb62444546c7f04a122ca2755b9628
Kai Shao <grepug@icloud.com>
2025-03-09 10:51:56 +0800

    Rename package and target from "epub-parser" to "EPUBParser" for consistency

--- Package.swift
+++ Package.swift
@@ -8,8 +8,8 @@ let package = Package(
     products: [
         // Products define the executables and libraries a package produces, making them visible to other packages.
         .library(
-            name: "epub-parser",
-            targets: ["epub-parser"])
+            name: "EPUBParser",
+            targets: ["EPUBParser"])
     ],
     dependencies: [
         .package(url: "https://github.com/weichsel/ZIPFoundation.git", .upToNextMajor(from: "0.9.0"))
@@ -18,7 +18,7 @@ let package = Package(
         // Targets are the basic building blocks of a package, defining a module or a test suite.
         // Targets can depend on other targets in this package and products from dependencies.
         .target(
-            name: "epub-parser",
+            name: "EPUBParser",
             dependencies: ["ZIPFoundation"]
         ),
         .testTarget(
